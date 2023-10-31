d4bfa29118d639ef9bcc44d88f08baa35a9efa9b
Anastasia Puchinina <puchinina.anastasia58@gmail.com>
2023-10-31 13:30:43 +0700

    Add new function and docstring

--- str_func.py
+++ str_func.py
@@ -1,5 +1,17 @@
 def upper_func(word):
     """
-    Function returns string with all capital letters
+    New docstring : function returns word with all capital letters
     """
     return word.upper()
+
+
+def title_func(string_input):
+    """
+    Functions returns string with first capital letters of each word
+    """
+    return string_input.title()
+
+
+
+
+
